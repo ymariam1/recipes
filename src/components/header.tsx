7632c8975597afd7ae2ac73a8ca126a33f598882
Yohannes Mariam <yohannesjrmariam@gmail.com>
2024-05-17 15:06:24 -0400

    Initial File Organization

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>AI-Powered Recipe Finder</h1>
+    {/* Add navigation links and auth buttons */}
+  </header>
+);
+
+export default Header;
