1137019344cfc1e22a36769fd93bef9d3217f2b0
Yohannes Mariam <yohannesjrmariam@gmail.com>
2024-05-20 17:16:23 -0400

    Implemented Google Auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token, user }) {
+      return session;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+  },
+});
